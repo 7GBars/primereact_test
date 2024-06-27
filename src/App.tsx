b5d2d38e6ef9662a6617336dc5ee7b805af689ca
Barsegyan <barsegyan@topsystems.ru>
2024-06-27 16:15:00 +0300

    -добавить button и TieredMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Button } from 'primereact/button';
+import {TieredMenu} from "primereact/tieredmenu";
+
+import {items} from "./__mocksData__/";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Button >test </Button>
+      <TieredMenu model={items} breakpoint="767px" />
     </div>
   );
 }
