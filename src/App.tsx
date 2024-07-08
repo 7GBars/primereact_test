7a61e2582adeb167d035650ecce8b0b638b12b6b
Barsegyan <barsegyan@topsystems.ru>
2024-07-08 12:00:10 +0300

    -вынести general в commonStyles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {TieredMenuTest} from "./components/TieredMenu/index";
 import {items} from "./__mocksData__/";
 
 import './App.css';
-
+import { PrimeIcons } from 'primereact/api';
 
 
 
@@ -15,7 +15,7 @@ function App() {
   const tieredMenuInstance = useRef<TieredMenu>(null)
   return (
     <div className="App">
-      <Button label="Show" onClick={(e) => tieredMenuInstance.current?.toggle(e)}/>
+      <Button icon={'pi pi-check'} label="Show" onClick={(e) => tieredMenuInstance.current?.toggle(e)}/>
       <TieredMenuTest model={items} instanceRefObject={tieredMenuInstance}/>
       <h1>2</h1>
       <p>dome text</p>
